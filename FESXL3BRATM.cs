1f95e765da5ef754c2db7b038909926043228b02
gav.davey <gav.davey@f324c46c-4524-3a04-90a4-8ce4bb0f4696>
2010-08-18 09:44:52 +0000

    Copy all strats from spare laptop, 180810. Backing up before writing new ATM cancel code for multi time frame strats.

---
+++ FESXL3BRATM.cs
@@ -0,0 +1,157 @@
+// 
+// Copyright (C) 2006, NinjaTrader LLC <www.ninjatrader.com>.
+// NinjaTrader reserves the right to modify or overwrite this NinjaScript component with each release.
+//
+
+#region Using declarations
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Xml.Serialization;
+using NinjaTrader.Cbi;
+using NinjaTrader.Data;
+using NinjaTrader.Indicator;
+using NinjaTrader.Strategy;
+#endregion
+
+
+// This namespace holds all strategies and is required. Do not change it.
+namespace NinjaTrader.Strategy
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Description("")]
+    public class FESXL3BRATM : Strategy
+    {
+        #region Variables
+		
+		private string	atmStrategyId		= string.Empty;
+		private string	orderId				= string.Empty;
+		
+		private int prevBarsPlusTicks = 1; // Default setting for PrevBarsPlusTicks
+		
+		private int barNumberOfOrder = 0; 
+		
+        #endregion
+
+        /// <summary>
+        /// This method is used to configure the strategy and is called once before any strategy method is called.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Print("43 Initialise FESX_3W_L3BR");
+			EntriesPerDirection = 1;
+			EntryHandling = EntryHandling.AllEntries; 
+			CalculateOnBarClose = false;
+			TraceOrders = true;
+        }
+		
+        /// <summary>
+        /// Called on each bar update event (incoming tick)
+        /// </summary>
+        protected override void OnBarUpdate()
+        {
+			// HELP DOCUMENTATION REFERENCE: Please see the Help Guide section "Using ATM Strategies"
+
+			// Make sure this strategy does not execute against historical data
+			if (Historical)
+				return;
+
+
+			// Submits an entry limit order at the current low price to initiate an ATM Strategy if both order id and strategy id are in a reset state
+            // **** YOU MUST HAVE AN ATM STRATEGY TEMPLATE NAMED 'AtmStrategyTemplate' CREATED IN NINJATRADER (SUPERDOM FOR EXAMPLE) FOR THIS TO WORK ****
+			if (Position.MarketPosition == MarketPosition.Flat 
+				&& orderId.Length == 0 
+				&& atmStrategyId.Length == 0 
+				&& FirstTickOfBar
+				&& Close [3] <= Open [3]
+				&& Close [2] <= Open [2]
+				&& Close [1] <= Open [1]
+				&& High  [1] + PrevBarsPlusTicks * TickSize > GetCurrentBid())
+			
+			
+			{
+				
+				
+				atmStrategyId = GetAtmStrategyUniqueId();
+				orderId = GetAtmStrategyUniqueId();
+				AtmStrategyCreate(Cbi.OrderAction.Buy, OrderType.Stop, 0, High[1] + PrevBarsPlusTicks * TickSize, TimeInForce.Day, orderId, "FESX_3W_L3BR", atmStrategyId);	
+			    barNumberOfOrder = CurrentBar;
+				Print("81 AtmStrategyCreate FESX_3W_L3BR");
+			}
+
+
+			// Check for a pending entry order
+			if (  orderId.Length > 0)
+			
+			
+			{
+				string[] status = GetAtmStrategyEntryOrderStatus(orderId);
+				
+				// If the status call can't find the order specified, the return array length will be zero otherwise it will hold elements
+
+				if (status.GetLength(0) > 0)
+					
+				
+				if (status[2] == "Filled" || status[2] == "Cancelled" || status[2] == "Rejected")
+				
+				{
+					Print("100 FESX_3W_L3BR Reset the strategy id if filled/cancelled or rejected");	
+      				orderId = string.Empty;
+				    					
+				}
+                
+				//if (Close [0] - 5 * TickSize > High[1] &&(status[2] == "Accepted" || status[2] == "Working" || status[2] == "Pending"))
+				if ((status[2] == "Accepted" || status[2] == "Working" || status[2] == "Pending") && High[1] > Close [0] - 5 * TickSize   )
+				
+				{
+				Print("108 ATM cancel FESX_3W_L3BR order");	
+				AtmStrategyCancelEntryOrder(orderId);
+				}
+			} 
+			
+						
+			if (atmStrategyId.Length > 0 && GetAtmStrategyMarketPosition(atmStrategyId) == MarketPosition.Flat)
+				{
+				Print("117 FESX_3W_L3BR reset the strategy id");
+				atmStrategyId = string.Empty;
+				orderId = string.Empty;
+				}
+
+
+			if (atmStrategyId.Length > 0)
+			{
+				
+				// Print some information about the strategy to the output window
+				Print("126 At the end of FESX_3W_L3BR");
+				Print("The current ATM Strategy market position is: " + GetAtmStrategyMarketPosition(atmStrategyId));
+				Print("The current ATM Strategy position quantity is: " + GetAtmStrategyPositionQuantity(atmStrategyId));
+				Print("The current ATM Strategy average price is: " + GetAtmStrategyPositionAveragePrice(atmStrategyId));
+				Print("The current ATM Strategy Unrealized PnL is: " + GetAtmStrategyUnrealizedProfitLoss(atmStrategyId));
+				Print("The current ATM Strategy Realized PnL is: " + GetAtmStrategyRealizedProfitLoss(atmStrategyId));
+			}
+        }
+	
+
+					
+			
+		
+	
+	
+        #region Properties
+		
+		[Description("")]
+        [GridCategory("Parameters")]
+        public int PrevBarsPlusTicks
+        {
+            get { return prevBarsPlusTicks; }
+            set { prevBarsPlusTicks = Math.Max(1, value); }
+        }
+		
+		
+        #endregion
+    }
+}
